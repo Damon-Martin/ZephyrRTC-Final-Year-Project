e110f3cfc92c9abaf7b5d50d1d2783ca49fe4f5d
Damon Martin <damon.martin@students.plymouth.ac.uk>
2023-11-27 14:49:52 +0000

    Reverse Proxy Progress

--- front-end/src/app/components/forms/input-box.tsx
+++ front-end/src/app/components/forms/input-box.tsx
@@ -1,7 +1,18 @@
-export default function InputBox() {
+interface InputBoxProps {
+    label: string;
+    type: string;
+    id: string;
+    name: string;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    required: boolean;
+};
+
+export default function InputBox({ label, type, id, name, value, onChange, required }: InputBoxProps): JSX.Element {
     return (
-        <div>
-            <label>Email <input type="text"/></label>
-        </div>
-    )
-}
+      <tr>
+        <td><label htmlFor={id}>{label}:</label></td>
+        <td><input type={type} id={id} name={name} value={value} onChange={onChange} required={required} /></td>
+      </tr>
+    );
+};
--- front-end/src/app/components/forms/login-form.tsx
+++ front-end/src/app/components/forms/login-form.tsx
@@ -1,12 +1,43 @@
+'use client'
+
+import { useState, ChangeEvent, FormEvent } from 'react';
 import InputBox from "./input-box";
 
 export default function LoginForm() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+  
+
+    const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value);
+    const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value);
+
+    const loginBtnPressed = (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      console.log("Email:", email);
+      console.log("Password:", password);
+    };
+
+    const regBtnPressed = (e) => {
+      e.preventDefault();
+      console.log("Registration Page");
+    };
+
     return (
-      <div>
-        <form>
-            <h3>Email</h3>
-            <InputBox></InputBox>
-        </form>
-      </div>
-    )
-}  
+      <form onSubmit={loginBtnPressed}>
+        <table>
+          <thead>
+            <tr>
+              <th>Login</th>
+            </tr>
+          </thead>
+          <tbody>
+            <InputBox label="Email" type="text" id="username" name="username" value={email} onChange={handleUsernameChange} required />
+            <InputBox label="Password" type="password" id="password" name="password" value={password} onChange={handlePasswordChange} required />
+            <tr>
+              <td><button type="submit">Login</button></td><td><button onClick={regBtnPressed}>Register</button></td>
+            </tr>
+          </tbody>
+        </table>
+    </form>
+    );
+};
