8acc4ba08aee65f53c9d79583c8417e2aecdd91a
Damon Martin <damon.martin@students.plymouth.ac.uk>
2023-11-27 12:55:32 +0000

    slight changes

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          // All api routes allowed
+          source: "/api/(.*)",
+          
+          headers: [
+            {
+              key: "Access-Control-Allow-Origin",
+              value: "*",
+            },
+            {
+              key: "Access-Control-Allow-Methods",
+              value: "GET, PUT, POST, PATCH, DELETE, OPTIONS",
+            },
+            {
+              key: "Access-Control-Allow-Headers",
+              value: "Content-Type, Authorization",
+            },
+          ],
+        }
+      ];
+    },
+};
 
 module.exports = nextConfig
