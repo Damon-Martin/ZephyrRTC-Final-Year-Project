9eb5db6d50ceb8434d2553de88105af5c4b3093b
Damon Martin <damon.martin@students.plymouth.ac.uk>
2023-11-30 13:09:50 +0000

    Login Screen: Background Color Gradient Added

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,9 +1,21 @@
+'use client'
+
 import LoginCard from "@/components/login/login-card";
 
 export default function Home() {
+  let loginPageStyle = {
+    minHeight: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    background: 'linear-gradient(to bottom, #FF00A8, #4542D8)',
+  };
+
   return (
-    <main className="container mx-auto flex items-center justify-center h-screen">
-      <LoginCard></LoginCard>
+    <main>
+      <div style={loginPageStyle}>
+        <LoginCard></LoginCard>
+      </div>
     </main>
   );
 }
