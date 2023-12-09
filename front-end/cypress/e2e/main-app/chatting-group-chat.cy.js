e9ba6986026f2b3f66754ad8eb243cd43bbdb5b3
Damon Martin <damon.martin@students.plymouth.ac.uk>
2023-12-09 23:44:16 +0000

    Planning tests

---
+++ front-end/cypress/e2e/main-app/chatting-group-chat.cy.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+describe('Messaging Group Chat E2E Tests', () => {
+  // Login to app
+  beforeEach(() => {
+    cy.visit('https://localhost/')
+  });
+
+  it('test-message-john-to-all', () => {
+    assert.fail('Test Not Made yet')
+  });
+
+  it('test-recieve-message-from-members', () => {
+    assert.fail('Test Not Made yet')
+  });
+
+  it('test-delete-msg-from-user', () => {
+    assert.fail('Test Not Made yet')
+  });
+
+  it('test-delete-msg-from-other-user-makes-msg-invisable', () => {
+    assert.fail('Test Not Made yet')
+  });
+
+  it('test-send-image', () => {
+    assert.fail('Test Not Made yet')
+  });
+
+  it('test-send-document', () => {
+    assert.fail('Test Not Made yet')
+  });
+});
---
+++ front-end/cypress/e2e/main-app/friends-page.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe('Friends E2E Tests', () => {
+  // Login to app
+  beforeEach(() => {
+    cy.visit('https://localhost/')
+  });
+
+  it('test-add-friend', () => {
+    assert.fail('Test Not Made yet')
+  });
+
+  it('test-remove-friend', () => {
+    assert.fail('Test Not Made yet')
+  });
+
+  it('test-remove-friend', () => {
+    assert.fail('Test Not Made yet')
+  });
+});
