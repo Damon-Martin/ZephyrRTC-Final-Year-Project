8e2c7ecf47128b923364892d69e6c737ec37934d
Damon Martin <damon.martin@students.plymouth.ac.uk>
2023-12-11 21:00:35 +0000

    Code Refactoring Main

--- auth-server/src/app/index.js
+++ auth-server/src/app/index.js
@@ -5,19 +5,31 @@ const { AuthRouter } = require('./routes/auth-router');
 const app = express();
 const port = 3000;
 
+// MongoDB Details
 const db_info = {
-  db_name: 'admin',
-  db_username: 'root',
-  db_pass: 'strong_password',
+    db_name: 'admin',
+    db_username: 'root',
+    db_pass: 'strong_password',
+    db_uri: function () {
+        return `mongodb://${this.db_username}:${this.db_pass}@mongo-db:27017/${this.db_name}`;
+    }
 }
 
-// MongoDB URI
-const db_uri = `mongodb://${db_info.db_username}:${db_info.db_pass}@mongo-db:27017/${db_info.db_name}`;
 
-// Routing
-const authRouter = new AuthRouter("test_params_working");
-app.use('/api/v1', authRouter.router );
+async function main(db_uri) {
+    try {
+        // Routing
+        const authRouter = new AuthRouter("test_params_working");
+        app.use('/api/v1', authRouter.router );
 
-app.listen(port, () => {
-  console.log(`Running on localhost:${port}`)
-})
+        app.listen(port, () => {
+            console.log(`Running on localhost:${port} & Connection String = ${db_info.db_uri()}`)
+        });
+    }
+    catch (e) {
+        console.error(e);
+    }
+}
+// Running Application
+main();
+
